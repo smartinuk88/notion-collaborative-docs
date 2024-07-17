358a5240865f2b017cdf9c9f1c8185c5f89ceaca
Scott Martin <scott.martin88@live.com>
2024-07-17 08:16:01 +0200

    Fix issue with Shadcn styling and build sheet menu

---
+++ components/Sidebar.tsx
@@ -0,0 +1,44 @@
+import { MenuIcon } from "lucide-react";
+import NewDocumentButton from "./NewDocumentButton";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+
+function Sidebar() {
+  const menuOptions = (
+    <>
+      <NewDocumentButton />
+
+      {/* My documents */}
+      {/* List... */}
+
+      {/* Shared with me */}
+      {/* List... */}
+    </>
+  );
+
+  return (
+    <div className="p-2 md:p-5 bg-gray-200 relative">
+      <div className="md:hidden">
+        <Sheet>
+          <SheetTrigger>
+            <MenuIcon className="p-2 hover:opacity-30 rounded-lg" size={40} />
+          </SheetTrigger>
+          <SheetContent side="left">
+            <SheetHeader>
+              <SheetTitle>Menu</SheetTitle>
+              <div>{menuOptions}</div>
+            </SheetHeader>
+          </SheetContent>
+        </Sheet>
+      </div>
+
+      <div className="hidden md:inline">{menuOptions}</div>
+    </div>
+  );
+}
+export default Sidebar;
