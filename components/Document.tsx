ebcfd2510e530a4b8f7f5d1e16617c95d6f67084
Scott Martin <scott.martin88@live.com>
2024-07-19 15:57:17 +0200

    Create useOwner custom hook

--- components/Document.tsx
+++ components/Document.tsx
@@ -5,11 +5,13 @@ import { doc, updateDoc } from "firebase/firestore";
 import { db } from "@/firebase";
 import { useDocumentData } from "react-firebase-hooks/firestore";
 import Editor from "./Editor";
+import useOwner from "@/lib/useOwner";
 
 function Document({ id }: { id: string }) {
   const [data, loading, error] = useDocumentData(doc(db, "documents", id));
   const [input, setInput] = useState("");
   const [isUpdating, startTransition] = useTransition();
+  const isOwner = useOwner();
 
   useEffect(() => {
     if (data) {
@@ -29,7 +31,7 @@ function Document({ id }: { id: string }) {
     }
   };
   return (
-    <div>
+    <div className="flex-1 h-full bg-white p-5">
       <div className="flex max-w-6xl mx-auto justify-between pb-5">
         <form className="flex flex-1 space-x-2" onSubmit={updateTitle}>
           <Input value={input} onChange={(e) => setInput(e.target.value)} />
