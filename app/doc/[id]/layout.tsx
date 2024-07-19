969c058225c376e2439609694914473c39743be5
Scott Martin <scott.martin88@live.com>
2024-07-19 08:08:12 +0200

    Implement follow pointer func with liveblocks

--- app/doc/[id]/layout.tsx
+++ app/doc/[id]/layout.tsx
@@ -1,4 +1,14 @@
-function layout() {
-  return <div>layout</div>;
+import RoomProvider from "@/components/RoomProvider";
+import { auth } from "@clerk/nextjs/server";
+
+function DocLayout({
+  children,
+  params: { id },
+}: {
+  children: React.ReactNode;
+  params: { id: string };
+}) {
+  auth().protect();
+  return <RoomProvider roomId={id}>{children}</RoomProvider>;
 }
-export default layout;
+export default DocLayout;
